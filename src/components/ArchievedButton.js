f2bc7458c698124af55a2c98f6689cbe30c3bed5
Abdul Hafidh <abdul_h20@mhs.unsyiah.ac.id>
2022-07-14 14:27:17 +0700

    fix a variable of the class

--- src/components/ArchievedButton.js
+++ src/components/ArchievedButton.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function ArchievedButton({ id, onArchieved }) {
-    return <button className='contact-item__archieved' onClick={() => onArchieved(id)}>Archieved</button>
+    return <button className='notebook-item__archieved' onClick={() => onArchieved(id)}>Archieved</button>
 }
 
 export default ArchievedButton;
