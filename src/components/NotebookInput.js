d10f3fdcdcff96abda0e6ecc3e4e4b1adb1a50f5
Abdul Hafidh <abdul_h20@mhs.unsyiah.ac.id>
2022-07-26 04:32:17 +0700

    fixing a bug on body

--- src/components/NotebookInput.js
+++ src/components/NotebookInput.js
@@ -9,12 +9,12 @@ class NotebookInput extends React.Component{
         // Deklarasi state
         this.state = {
             title: "",
-            description: ""
+            body: ""
         }
 
         // Call bind
         this.onTitleChangeEventHandler = this.onTitleChangeEventHandler.bind(this);
-        this.onDescriptionChangeEventHandler = this.onDescriptionChangeEventHandler.bind(this);
+        this.onBodyChangeEventHandler = this.onBodyChangeEventHandler.bind(this);
         this.onSubmitEventHandler = this.onSubmitEventHandler.bind(this);
 
     }
@@ -28,11 +28,11 @@ class NotebookInput extends React.Component{
         });
     }
 
-    onDescriptionChangeEventHandler(event){
+    onBodyChangeEventHandler(event){
         this.setState((prevState) => {
             return {
                 ...prevState,
-                description: event.target.value
+                body: event.target.value
             }
         });
     }
@@ -46,7 +46,7 @@ class NotebookInput extends React.Component{
         return (
             <form className="notebook-input" onSubmit={this.onSubmitEventHandler}>
                 <input type="text" required placeholder="title" value={this.state.title} onChange={this.onTitleChangeEventHandler}/> <br></br>
-                <input type="textarea"  required placeholder="description" value={this.state.description} onChange={this.onDescriptionChangeEventHandler}/> <br />
+                <input type="textarea"  required placeholder="description" value={this.state.body} onChange={this.onBodyChangeEventHandler}/> <br />
                 <button type="submit" className="notebook-input__button">Add Notebook</button>
             </form>
         );
